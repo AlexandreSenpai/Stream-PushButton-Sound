a6df2b38d42be88a10cfb5a4a1eac967bbaa6caa
Sora <alexandrebsramos@hotmail.com>
2019-04-14 19:38:14 -0300

    Parar Sounds e Tocar Effects

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from playEffect import *
 
 if __name__ == '__main__':
   while True:
-    sound_effect('d', 0)
-    sound_effect('1', 1)
+    play_effects('d', 0)
+    play_sound('1', 0)
--- playEffect.py
+++ playEffect.py
@@ -1,5 +1,4 @@
 import winsound
-from time import sleep
 from playsound import playsound
 import keyboard
 import json
@@ -9,15 +8,20 @@ def read_json():
     data = json.load(_dir)
   return data
 
-def sound_effect(key, id):
+def play_sound(key, id):
   sounds = read_json()
   if keyboard.is_pressed(key):
     winsound.PlaySound(sounds['tracks'][id]['dir'], winsound.SND_ASYNC)
-    sleep(0.09)
 
   if keyboard.is_pressed('g'):
     winsound.PlaySound(None, winsound.SND_ASYNC)
+
+def play_effects(key, id):
+  sounds = read_json()
+  if keyboard.is_pressed(key):
+    playsound(sounds['effects'][id]['dir'])
     
 if __name__ == '__main__':
   while True:
-    sound_effect('d', 0)
+    play_effects('1', 0)
+    play_sound('d', 0)
